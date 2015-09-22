6265b1bc46edbca7fbb2b8a5add66cf0004a7b34
Sashwat <Sashwat@10.0.0.12>
2015-09-22 12:31:15 -0500

    maps chnages

---
+++ standalone/src/standalone/Maps.java
@@ -0,0 +1,38 @@
+package standalone;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Maps {
+	public  static void main(String[] args)
+	{
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+		
+		testMap(linkedHashMap);
+		
+	}
+	
+	static void testMap(Map<Integer, String> map)
+	{
+		map.put(4, "four");
+		map.put(3, "three");
+		map.put(7, "seven");
+		map.put(1, "one");
+		
+		for(Map.Entry<Integer, String> entry: map.entrySet())
+		{
+			System.out.println(entry.getKey() +": "+entry.getValue());
+		}
+		System.out.println();
+		for(Integer key : map.keySet())
+		{
+			String value = map.get(key);
+			System.out.println(key+": "+value);
+		}
+	}
+
+}
